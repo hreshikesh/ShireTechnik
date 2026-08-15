5177bec7fc51dfc2759a7b9c3dc3eb18c3c5b58c
Hreshikesh <ailhreshikesh@gmail.com>
2026-08-15 15:34:37 +0530

    updated shiretechnick with teamdata

---
+++ shiretechnik-frontend/src/data/teamData.js
@@ -0,0 +1,59 @@
+import about from "../assets/images/about/man.webp";
+import debasisImage from "../assets/images/about/debasis.webp";
+import shankarImage from "../assets/images/about/shankar.webp";
+import man from "../assets/images/about/man.webp"
+import snadeep from "../assets/images/about/sandeep.webp";
+import pranjal from "../assets/images/about/pranjal.webp";
+import pradeep from "../assets/images/about/pradeep.webp";
+import archan from "../assets/images/about/archan.webp";
+export const teamData = [
+  {
+    id: 1,
+    name: "Debasis Panda",
+    designation: "Founder Director,CEO",
+    image: debasisImage,
+    linkedin: "https://www.linkedin.com/in/debasis-panda-a87a245/"
+  },
+  {
+    id: 2,
+    name: "Shankar S",
+    designation: "Director",
+    image: shankarImage,
+    linkedin: "https://www.linkedin.com/in/shankar-s-ba388533/"
+  },
+  {
+    id: 3,
+    name: "Archan Chatterjee",
+    designation: "Director",
+    image:archan ,
+    linkedin: ""
+  },
+  {
+    id: 4,
+    name: "Sandeep Nandi",
+    designation: "Principal Consultant IT and AI",
+    image: snadeep,
+    linkedin: "https://www.linkedin.com/in/sandeep-nandi-06409310/"
+  },
+   {
+    id: 5,
+    name: "Pranjal Mishra",
+    designation: "CAE Engineer",
+    image: pranjal,
+    linkedin: "https://www.linkedin.com/in/pranjal-m11/"
+  },
+   {
+    id: 6,
+    name: "Pradeep Kunta",
+    designation: "Service Engineer",
+    image: pradeep,
+    linkedin: "https://www.linkedin.com/in/pradeep-reddy-883b6715b/"
+  },
+   {
+    id: 7,
+    name: "Sarang Shidore",
+    designation: "Advisory Board Member",
+    image: man,
+    linkedin: "https://www.linkedin.com/in/sandeep-nandi-06409310/"
+  }
+];
--- shiretechnik-frontend/src/feactures/about/team/Team.jsx
+++ shiretechnik-frontend/src/feactures/about/team/Team.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-import { teamData } from "../../aboutHome/teamData";
+import { teamData } from "../../../data/teamData";
 
 const LinkedinIcon = ({ size = 18, className = "" }) => (
   <svg
